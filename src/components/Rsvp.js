3d7670e71dab775a97befe2b6dc1e8a9d9ae492a
Mel Wightman <mel.wightman@outlook.com>
2018-04-02 09:32:39 -0400

    weddingId being passed properly now

--- src/components/Rsvp.js
+++ src/components/Rsvp.js
@@ -9,11 +9,10 @@ class Rsvp extends React.Component {
   }
 
 refresh = () => {
-  // get all block guests from the backend 
-  //axios.get(`/guests/${this.props.weddingInfo._id}`)
-  //PROPS AREN'T BEING PASSED NEED HELP WITH THIS
-  //debugger
-  axios.get("/guests/5ab8149fb885752250cd531d")
+  // this.refresh is being passed into CreateGuest and I'm unsure how this will work?
+  // I also need to pass in weddingInfo._id into CreateGuest since on Post I need to link the guest to the wedding.
+  
+  axios.get(`/guests/${this.props.weddingInfo._id}`)
   .then(res => {
     const data = res.data;
     // if blog guests come back
@@ -22,7 +21,22 @@ refresh = () => {
       this.setState({ guests: data.payload });
     }
   });
+
 };
+
+  componentWillReceiveProps(nextProps) {
+     if (nextProps.weddingInfo._id) {
+         axios.get(`/guests/${nextProps.weddingInfo._id}`)
+           .then(res => {
+              const data = res.data;
+              // if blog guests come back
+              if (data.payload) {
+                // store them in state
+                this.setState({ guests: data.payload });
+              }
+            });
+     }
+  }
 
   componentDidMount() {
     this.refresh();
@@ -31,7 +45,7 @@ refresh = () => {
   render() {
     return (
       <div>
-          <CreateGuest refresh={this.refresh} />
+        <CreateGuest refresh={this.refresh} weddingId={this.props.weddingInfo._id}/>
         <div className="guests">
           {this.state.guests.map( guest => <Guest key={guest._id}{...guest} />)}
           </div>
